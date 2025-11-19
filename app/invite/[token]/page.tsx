327a0a447afc47fe36eb5860108be87636b3df68
shyaradigital <shyaradigital@gmail.com>
2025-11-19 10:56:58 +0530

    Fix missing motion import and move eslint to dependencies

--- app/invite/[token]/page.tsx
+++ app/invite/[token]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useParams, useRouter } from 'next/navigation'
+import { motion } from 'framer-motion'
 import { generateDeviceFingerprint } from '@/lib/device-fingerprint'
 import PhoneVerificationForm from '@/components/PhoneVerificationForm'
 import AccessRestrictedPopup from '@/components/AccessRestrictedPopup'
