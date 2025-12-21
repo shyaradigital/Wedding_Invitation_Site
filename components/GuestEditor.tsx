e1918b1c63e84157e2377738cfb6cab6a9a339e5
shyaradigital <shyaradigital@gmail.com>
2025-12-21 11:03:25 +0530

    Fix ESLint error: escape apostrophe in JSX

--- components/GuestEditor.tsx
+++ components/GuestEditor.tsx
@@ -2546,7 +2546,7 @@ export default function GuestEditor({
 
                   <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4">
                     <p className="text-sm text-yellow-800">
-                      <strong>Note:</strong> This will send the email to all guests who have an email address. The variables {'{{'}params.guestName{'}}'} and {'{{'}params.inviteLink{'}}'} will be automatically replaced with each guest's information.
+                      <strong>Note:</strong> This will send the email to all guests who have an email address. The variables {'{{'}params.guestName{'}}'} and {'{{'}params.inviteLink{'}}'} will be automatically replaced with each guest&apos;s information.
                     </p>
                   </div>
 
